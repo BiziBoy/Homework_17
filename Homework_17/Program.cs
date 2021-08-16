e9a5aa512e3178aacbffee69da647266eb4c1cd2
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2021-08-16 14:51:56 +0300

    Добавьте файлы проекта.

---
+++ Homework_17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//рекурсивный метод, который находит сумму цифр в числе
+namespace Homework_17
+{
+  class Program
+  {
+    static int SumNumbers(int value)
+    {
+      if (value < 10)
+      {
+        return value;
+      }
+      int part = (value - value % 10) / 10;
+      return SumNumbers(part) + 
+    }
+    static void Main(string[] args)
+    {
+    }
+  }
+}
